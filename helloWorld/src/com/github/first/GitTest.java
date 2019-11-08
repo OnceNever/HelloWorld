1af2d9486064c572efd62906dccd399aeafe087b
walkonbench <1269123897@qq.com>
2019-11-08 14:38:28 +0800

    first test

---
+++ helloWorld/src/com/github/first/GitTest.java
@@ -0,0 +1,11 @@
+package com.github.first;
+/**
+ * The first test for @github
+ * @author walkonbench
+ *
+ */
+public class GitTest {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
